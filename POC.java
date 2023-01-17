57bd9b7f0faa8ac052d9ee9818f0b39257622cf5
sanvygithub <122703271+sanvygithub@users.noreply.github.com>
2023-01-17 22:39:07 +0530

    Create POC.java

---
+++ POC.java
@@ -0,0 +1,19 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class POC {
+	
+	
+	By cart=By.id("poc");
+	
+	public POC() {
+		
+	}
+	
+	public void pocMethod() {
+		System.out.println("poc");
+		
+	}
+
+}
