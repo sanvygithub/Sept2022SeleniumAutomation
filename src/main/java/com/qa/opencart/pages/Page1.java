82b4a4480a49d958699f4a99f65ff10723cb8036
Sandhya kakarla <sandhyakakarla1462@gmail.com>
2023-01-18 21:58:05 +0530

    page1 feature1 code added

--- src/main/java/com/qa/opencart/pages/Page1.java
+++ src/main/java/com/qa/opencart/pages/Page1.java
@@ -3,7 +3,8 @@ package com.qa.opencart.pages;
 public class Page1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("feature 1.0");
 
 	}
 
